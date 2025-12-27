9d7bfa09f9a4646675062e0416d37e0b1874cc71
Likitha H E <likithahe456@gmail.com>
2025-12-27 11:23:53 +0530

    new method added

--- src/test/java/test/LikithaTest.java
+++ src/test/java/test/LikithaTest.java
@@ -8,12 +8,13 @@ public class LikithaTest {
 		System.out.println("Hi Geetha....");
 		System.out.println("How are you?");
 		System.out.println("Have good day");
-		
+
 	}
-	
+
 	@Test
 	public void test2() {
 		System.out.println("What is new year plan Geetha!!!!");
+		System.out.println("what's your plan for today");
 	}
 
 }
